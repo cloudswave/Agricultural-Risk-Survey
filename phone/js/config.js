ced47d94fc64b6f4990576d97d678b1334f154cb
ethanzhu <764724624@qq.com>
2015-02-05 23:33:26 +0800

    增加设置接口地址功能，添加受灾成数字段

--- phone/js/config.js
+++ phone/js/config.js
@@ -2,3 +2,11 @@
 var client_http="http://php.xlanlab.com/sxzy/index.php?g=agro&m=client";
 var uploadHttp =  "http://php.xlanlab.com/sxzy/upload/index.php";
 var img_path="http://php.xlanlab.com/sxzy/upload/img/";
+
+var form_setting=getStorJson("form_setting");
+if(isDefine(form_setting.server_http)){
+     client_http=form_setting.server_http+"/index.php?g=agro&m=client";
+     uploadHttp = form_setting.server_http+"/upload/index.php";
+     img_path= form_setting.server_http+"/upload/img/";
+}
+
